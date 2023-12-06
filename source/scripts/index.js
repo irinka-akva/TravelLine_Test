58475544ba041d3bc9b0bc1523c2c193cb333a3a
Ирина Украинцева <irina@192.168.1.6>
2023-12-06 22:47:04 +0300

    Вносит правки в код

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,20 +1,16 @@
 const catalog = document.querySelector(".catalog__list");
 
-const setReservation = (card, method, boolean, tabindex) => {
+const setReservation = (card, method, isDisabled, tabindex) => {
   card.classList[method]("reserved");
-  card.querySelector(".card__button").disabled = boolean;
+  card.querySelector(".card__button").disabled = isDisabled;
   card.querySelector(".card__title-link").setAttribute("tabindex", tabindex);
 };
 
 const getCardReserved = (evt) => {
   const parentCard = evt.target.closest(".card");
   let relatedTarget = evt.relatedTarget;
-
   if (!parentCard.contains(relatedTarget)) {
     setReservation(parentCard, "add", true, "-1")
-    // parentCard.classList.add("reserved");
-    // parentCard.querySelector(".card__button").disabled = true;
-    // parentCard.querySelector(".card__title-link").setAttribute("tabindex", "-1");
     parentCard.removeEventListener("mouseout", getCardReserved);
   }
 };
@@ -24,12 +20,8 @@ const setStatusOfReserve = (evt) => {
 
   if (evt.target.closest(".card__button")) {
     parentCard.addEventListener("mouseout", getCardReserved);
-
-  } else if (evt.target.closest(".card__booked") && !evt.target.closest(".card__booked-link")) {
+  } else if (evt.target.closest(".card__booked") || evt.target.closest(".card__booked-wrapper") && !evt.target.closest(".card__booked-link")) {
     setReservation(parentCard, "remove", false, "0")
-    // parentCard.classList.remove("reserved");
-    // parentCard.querySelector(".card__button").disabled = false;
-    // parentCard.querySelector(".card__title-link").setAttribute("tabindex", "0");
   }
 };
 
